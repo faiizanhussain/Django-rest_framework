0fe40352455b753e366e9de43e920f6391ea5857
Faizan Hussain <faiizanhussain@gmail.com>
2023-10-05 15:44:25 +0500

    model added

--- quickStart/models.py
+++ quickStart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title    
